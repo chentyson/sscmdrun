12ea5861cc84da64c68548b23b480703e3cc084b
chentyson <1716677@qq.com>
2018-11-03 19:57:36 -0700

    fix

---
+++ update/price.sql
@@ -0,0 +1,10 @@
+insert into price(id,name,ips,devs,year,halfyear,quarter,month) values(1,'单设备个人版',1,1,180,110,60,25);
+insert into price(id,name,ips,devs,year,halfyear,quarter,month) values(2,'个人版',2,2,300,180,100,40);
+insert into price(id,name,ips,devs,year,halfyear,quarter,month) values(5,'5设备独立IP企业版',5,5,750,450,250,100);
+insert into price(id,name,ips,devs,year,halfyear,quarter,month) values(6,'8设备独立IP企业版',8,8,1100,660,360,150);
+insert into price(id,name,ips,devs,year,halfyear,quarter,month) values(7,'10设备独立IP企业版',10,10,1350,800,450,180);
+insert into price(id,name,ips,devs,year,halfyear,quarter,month) values(8,'15设备独立IP企业版',15,15,2000,1200,650,260);
+insert into price(id,name,ips,devs,year,halfyear,quarter,month) values(9,'20设备独立IP企业版',20,20,2500,1500,820,330);
+insert into price(id,name,ips,devs,year,halfyear,quarter,month) values(10,'30设备独立IP企业版',30,30,3000,1800,980,400);
+insert into price(id,name,ips,devs,year,halfyear,quarter,month) values(11,'40设备独立IP企业版',40,40,3500,2100,1150,460);
+
