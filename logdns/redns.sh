162de29dbfbce190ba0ab39ca26fa931a0bfa375
chentyson <1716677@qq.com>
2019-11-17 16:47:06 +0800

    fix

--- logdns/redns.sh
+++ logdns/redns.sh
@@ -3,17 +3,17 @@ file=/etc/nginx/nginx.conf
 dir=/root/sscmdrun/logdns
 
 if [ ! -f "$dir/$1.txt" ]; then
-    echo 'no oldip file $dir/$1.txt found!'
+    echo $(date) 'no oldip file $dir/$1.txt found!'>>$dir/redns.log
     exit
 fi
 oldip=`cat $dir/$1.txt`
-echo old is $oldip
+#echo old is $oldip
 
 newip=`echo $2 | sed 's/\./\\\./g'`
 count=`grep -w $newip $file | wc -l`
-echo count is $count
+#echo count is $count
 if [ $count -gt 0 ]; then
-    echo 'ip count is great than 0, multiple operation~ ignore'
+    echo $(date) 'ip count is great than 0, multiple operation~ ignore'>>$dir/redns.log
     exit
 fi
 
@@ -23,6 +23,9 @@ sed -i 's/'$oldip'/'$2'/g' $file
 echo $2>$dir/$1.txt
 #log
 echo $(date) 'replace file:'$file' that '$oldip' with '$2' for '$1>>$dir/redns.log
-echo replace $oldip with $2 for $1,done
+#echo replace $oldip with $2 for $1,done
 
+echo $(date) 'restarting nginx...'>>$dir/redns.log
 systemctl restart nginx
+echo $(date) 'restarted nginx.'>>$dir/redns.log
+
